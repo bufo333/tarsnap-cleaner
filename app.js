6b057fafb7887698718e42cb2c1e817985d970e8
John Burns <bufo333@gmail.com>
2016-03-26 16:25:45 -0400

    added cli messages to app loader

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+'use strict';
 /**
  * Hello world
  * @param Object object
  * @return String
  */
-
+let args =[];
 
  // print process.argv
  process.argv.forEach(function(val, index, array) {
      args = array;
-     if (args.length!>2){
-       console.log("\nThe first argument should be the length of time in days \n");
-       console.log("of the oldest backup. If an argument is not entered a\n");
-       console.log("default of 30 days will be used. All backups older than\n");
-       console.log("30 days will be deleted!");
-     }
  });
 
+/*
+if(process.argv.indexOf("-h") != -1){ //does our flag exist?
+    city = process.argv[process.argv.indexOf("-c") + 1]; //grab the next item
+}
+*/
+
+if((process.argv.indexOf("-h") != -1) || args.length <3){
+   console.log('Usage:');
+   console.log('\ntsclean <command>\n');
+   console.log("-m:\t Monthly valid input in months 1-12.\n\t Delete backups older than specified number.\n");
+   console.log("-d:\t Daily valid input in days valid input 1-365.\n\t Deletes backups older than specified number.\n");
+   console.log("-y:\t Yearly valid input in years 1-10.\n\t Deletes backups older than specified number.");
+ };
  console.log(args.slice(2));
+ console.log(process.argv);
